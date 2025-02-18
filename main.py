7cdd3cb814a476df0fa6fdd823874b3e131fb421
geveev <144543740+geveev@users.noreply.github.com>
2025-02-18 14:31:07 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+import os
+import zipfile
+
+for i in range(10, 200):
+    filename = "file%d" % i
+    f = open('random_files/' + filename, 'rb').read(3)
+    if f == (b'\xff\xd8\xff'):
+        os.rename('random_files/' + filename, 'random_files/' + filename + ".jpg")
+    else:
+        os.remove('random_files/' + filename)
